003dc8c6a75649065530ddb688f2dee049cae15e
Pavel Sarwar <pavelsarwar@users.noreply.github.com>
2026-08-16 14:27:19 +0800

    Show three upcoming events on homepage

--- events-config.js
+++ events-config.js
@@ -1,10 +1,10 @@
 window.OVERSEAS_EVENTS_CONFIG = {
   sheetId: '1pm9ddbRRolyTJJlnpNNjly-urC4LjziiRKIOKmBDHoE',
   gid: '0',
-  homepageLimit: 2,
+  homepageLimit: 3,
   // Current Google Sheet headers:
-  // Title | Start Date | Start Time | End Date | End Time | Location | description
+  // Title | Start Date | Start Time | End Date | End Time | Location | description | registration_link
   // Optional future columns supported by the site can be added after these:
-  // RegisterURL | Category
+  // Category
   timezone: 'Asia/Kuala_Lumpur'
 };
